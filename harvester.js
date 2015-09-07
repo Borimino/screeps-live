8d4b2f34f26b00cfd80dc01b42e8c62c641490ee
Borimino <markusandreassen1994@gmail.com>
2015-09-07 20:17:33 +0200

    Changed storage of source to storage of sourceID

--- harvester.js
+++ harvester.js
@@ -1,20 +1,20 @@
 module.exports = function(creep)
 {
-	var source;
-	if (creep.memory.source == null)
-	{
-		source = creep.room.find(FIND_SOURCES)[0];
-		creep.memory.source = source;
-	} else
-	{
-		source = creep.memory.source;
-	}
+    var source;
+    if (creep.memory.sourceID == null)
+    {
+        source = creep.room.find(FIND_SOURCES)[0];
+        creep.memory.sourceID = source.id;
+    } else
+    {
+        source = Game.getObjectById(creep.memory.sourceID);
+    }
     
     
     
 	//var source = creep.room.find(FIND_SOURCES)[0];
     console.log(source.pos.x);
     console.log(source.pos.y);
-    console.log(creep.moveTo(source));
-    creep.harvest(source);
+    console.log(creep.moveTo(source.pos.x, source.pos.y));
+    console.log(creep.harvest(source));
 }

53e312e5809a748f9418501e79b28d854168b0b7
Borimino <markusandreassen1994@gmail.com>
2015-09-07 20:01:18 +0200

    Removed storage of source

--- harvester.js
+++ harvester.js
@@ -1,18 +1,18 @@
 module.exports = function(creep)
 {
-    var source;
-    if (creep.memory.source == null)
-    {
-        source = creep.room.find(FIND_SOURCES)[0];
-        creep.memory.source = source;
-    } else
-    {
-        source = creep.memory.source;
-    }
+    //var source;
+    //if (creep.memory.source == null)
+    //{
+        //source = creep.room.find(FIND_SOURCES)[0];
+        //creep.memory.source = source;
+    //} else
+    //{
+        //source = creep.memory.source;
+    //}
     
     
     
-    //var source = creep.room.find(FIND_SOURCES)[0];
+	var source = creep.room.find(FIND_SOURCES)[0];
     console.log(source.pos.x);
     console.log(source.pos.y);
     console.log(creep.moveTo(source));
